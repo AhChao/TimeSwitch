10ccef2fe84ffa26cb480bc1405b3bff501e176e
stevne.chao <gash612@gmail.com>
2022-07-20 00:27:36 +0800

    [Feat] Make items able to edit name

--- src/index.js
+++ src/index.js
@@ -25,16 +25,15 @@ const app = Vue.createApp({
 
         },
         convertToDisplayTime(timeInSecond) {
-            let timeStr = "";
-            let counting = 0;
-            while (timeInSecond > 60) {
-                counting++;
-                let temp = timeInSecond / 60;
-                timeStr += (temp < 10 ? "0" : "") + temp + ":";
-            }
-            timeStr += (timeInSecond < 10 ? "0" : "") + timeInSecond;
+            var sec_num = parseInt(timeInSecond, 10); // don't forget the second param
+            var hours = Math.floor(sec_num / 3600);
+            var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+            var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-            return (counting == 0 ? "00:" : "") + (counting == 1 ? "00:" : "") + timeStr;
+            if (hours < 10) { hours = "0" + hours; }
+            if (minutes < 10) { minutes = "0" + minutes; }
+            if (seconds < 10) { seconds = "0" + seconds; }
+            return hours + ':' + minutes + ':' + seconds;
         },
         newItem() {
             let newId = Math.max(...this.itemList.map(x => x.itemId)) + 1;
