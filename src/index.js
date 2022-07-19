35e808cdffc1c7b4d4d2bdb74bf4efa66253f9b1
stevne.chao <gash612@gmail.com>
2022-07-20 00:04:10 +0800

    [Feat] Basic function on timer

--- src/index.js
+++ src/index.js
@@ -2,27 +2,39 @@ const app = Vue.createApp({
     created() { },
     data() {
         return {
+            timer: {
+                timerTarget: "",
+                timerInstance: null,
+            },
             itemList: [{
                 "itemId": 0,
                 "itemName": "Read related material",
                 "totalSecond": 0,
-                "bgColor": this.getRandomColor()
+                "bgColor": this.getRandomColor(),
             }],
         };
     },
     mounted() {
+        this.init();
     },
     computed: {
     },
     methods: {
+        init() {
+            console.log("page init");
+
+        },
         convertToDisplayTime(timeInSecond) {
             let timeStr = "";
+            let counting = 0;
             while (timeInSecond > 60) {
+                counting++;
                 let temp = timeInSecond / 60;
-                timeStr += temp + ":";
+                timeStr += (temp < 10 ? "0" : "") + temp + ":";
             }
-            timeStr += timeInSecond;
-            return timeStr;
+            timeStr += (timeInSecond < 10 ? "0" : "") + timeInSecond;
+
+            return (counting == 0 ? "00:" : "") + (counting == 1 ? "00:" : "") + timeStr;
         },
         newItem() {
             let newId = Math.max(...this.itemList.map(x => x.itemId)) + 1;
@@ -35,6 +47,21 @@ const app = Vue.createApp({
                     "temp": ""
                 }
             );
+        },
+        startTimer() {
+            clearInterval(this.timer.timerInstance);
+            this.timer.timerInstance = setInterval(this.timerDoSth, 1000);
+        },
+        stopTimer() {
+            clearInterval(this.timer.timerInstance);
+        },
+        timerDoSth() {
+            if (this.itemList.map(x => x.itemId).indexOf(this.timer.timerTarget) == -1) return;
+            this.itemList.filter(x => x.itemId == this.timer.timerTarget)[0].totalSecond++;
+        },
+        changeTimerTarget(id) {
+            this.timer.timerTarget = id;
+            this.startTimer();
         },
         removeItem(itemId) {
             this.itemList = this.itemList.filter(x => x.itemId != itemId);
